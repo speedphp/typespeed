2278ae5abf0ccdd8ba3fd67f2940efb40e3633c7
speedphp <jake@speedphp.com>
2022-09-18 23:52:55 +0800

    但是，为什么main里面执行最后的log语句，会是在第一行输出呢？

--- test/main.ts
+++ test/main.ts
@@ -1,3 +1,4 @@
+import { log } from "../src/speed";
 import * as walkSync from "walk-sync";
 
 const srcDir = process.cwd() + "/src";
@@ -11,4 +12,5 @@ const testPaths = walkSync(testDir, { globs: ['**/*.ts'] });
 for(let p of testPaths) {
     import(testDir + "/" + p);
 }
-console.log("Main file running...");
+
+log("Main file running...");
