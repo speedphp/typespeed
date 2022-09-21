4fe16097ae7258d69aa93cf3adeebdf52f87c43e
speedphp <jake@speedphp.com>
2022-09-21 22:33:47 +0800

    start和app修饰器，作为一个程序的启动入口，很合理。

--- test/main.ts
+++ test/main.ts
@@ -1,16 +1,12 @@
-import { log } from "../src/speed";
-import * as walkSync from "walk-sync";
+import app from "../src/app.decorator";
+import start from "../src/start.decorator";
 
-const srcDir = process.cwd() + "/src";
-const srcPaths = walkSync(srcDir, { globs: ['**/*.ts'] });
-for(let p of srcPaths) {
-    import(srcDir + "/" + p);
+
+@start
+class Main {
+
+    @app
+    public main(){
+        console.log("start")
+    }
 }
-
-const testDir = process.cwd() + "/test";
-const testPaths = walkSync(testDir, { globs: ['**/*.ts'] });
-for(let p of testPaths) {
-    import(testDir + "/" + p);
-}
-
-log("Main file running...");
