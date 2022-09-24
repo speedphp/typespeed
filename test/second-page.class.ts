985a9b051e1d742c6c24359d013eb10ccb5b44c2
speedphp <jake@speedphp.com>
2022-09-25 00:19:58 +0800

    测试cookie和session

---
+++ test/second-page.class.ts
@@ -0,0 +1,24 @@
+import { log, onClass } from "../src/speed";
+import { GetMapping } from "../src/route-mapping.decorator";
+
+@onClass
+export default class SecondPage {
+
+    @GetMapping("/second/setCookie")
+    setCookiePage(req, res) {
+        res.cookie("name", "zzz");
+        return "setCookie";
+    }
+
+    @GetMapping("/second/getCookie")
+    getCookiePage(req, res) {
+        const cookieName = req.cookies.name;
+        return "getCookie: " + cookieName;
+    }
+
+    @GetMapping("/second/testSession")
+    testForSession(req, res) {
+        req.session.view = req.session.view ? req.session.view + 1 : 1;
+        return "testForSession: " + req.session.view;
+    }
+}
