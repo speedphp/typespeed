97d99148bd91f4e8ab5b6c296099accf0ed260e2
speedphp <jake@speedphp.com>
2022-09-25 00:29:22 +0800

    使用multiparty初步实现上传文件

--- test/second-page.class.ts
+++ test/second-page.class.ts
@@ -1,5 +1,7 @@
+import * as multiparty from "multiparty";
+import * as util from "util"
 import { log, onClass } from "../src/speed";
-import { GetMapping } from "../src/route-mapping.decorator";
+import { GetMapping, PostMapping } from "../src/route-mapping.decorator";
 
 @onClass
 export default class SecondPage {
@@ -20,5 +22,22 @@ export default class SecondPage {
     testForSession(req, res) {
         req.session.view = req.session.view ? req.session.view + 1 : 1;
         return "testForSession: " + req.session.view;
+    }
+
+    @PostMapping("/upload")
+    public upload(req, res) {
+        const form = new multiparty.Form();
+
+        form.parse(req, (err, fields, files) => {
+            res.writeHead(200, { 'content-type': 'text/plain' });
+            res.write('received upload:\n\n');
+            log(files);
+            res.end(util.inspect({ fields: fields, files: files }));
+        });
+    }
+
+    @GetMapping("/form")
+    form(req, res) {
+        res.render("upload");
     }
 }
