e9f5d179efdc4be1f78fc31bc958fcf9796efc43
speedphp <jake@speedphp.com>
2022-09-24 23:19:01 +0800

    用consolidate完成多模版引擎的支持

--- src/default/express-server.class.ts
+++ src/default/express-server.class.ts
@@ -1,4 +1,5 @@
 import * as express from "express";
+import * as consolidate from "consolidate";
 import ServerFactory from "../factory/server-factory.class";
 import { setRouter } from "../route-mapping.decorator";
 import { bean, log } from "../speed";
@@ -6,19 +7,35 @@ import { bean, log } from "../speed";
 export default class ExpressServer extends ServerFactory {
     @bean
     public getSever(): ServerFactory {
-        return new ExpressServer();
+        const server = new ExpressServer();
+        server.app = express();
+        return server;
     }
+
     public setMiddleware(middleware: any) {
         this.middlewareList.push(middleware);
     }
+
     public start(port: number) {
-        const app: express.Application = express();
         this.middlewareList.forEach(middleware => {
-            app.use(middleware);
+            this.app.use(middleware);
         });
-        setRouter(app);
-        app.listen(port, () => {
+        this.setDefaultMiddleware();
+        this.app.listen(port, () => {
             log("server start at port: " + port);
         });
+    }
+
+    private setDefaultMiddleware() {
+        const viewConfig = {
+            "engine": "mustache",
+            "path": "/test/views",
+            "suffix": "html"
+        };
+        this.app.engine(viewConfig["suffix"], consolidate[viewConfig["engine"]]);
+        this.app.set('view engine', viewConfig["suffix"]);
+        this.app.set('views', process.cwd() + viewConfig["path"]);
+
+        setRouter(this.app);
     }
 }
