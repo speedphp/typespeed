3f21cd69d82b0e4f3e2079beaad55c840356cb4a
speedphp <jake@speedphp.com>
2022-09-24 23:24:20 +0800

    测试配置文件覆盖

--- test/main.ts
+++ test/main.ts
@@ -11,5 +11,11 @@ class Main {
     public main(){
         this.server.start(8080);
         log('start application');
+
+        const config = require("./config.json");
+        const testConfig = require("./config-test.json");
+
+        let realConfig = Object.assign(config, testConfig);
+        console.log(realConfig);
     }
 }
