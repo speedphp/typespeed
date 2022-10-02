8b96bf1e70b48d23c1b84a9db1d67a0895033e5f
speedphp <jake@speedphp.com>
2022-10-02 17:13:27 +0800

    测试从SQL内获取表名

---
+++ src/testGetTable.ts
@@ -0,0 +1,17 @@
+
+
+const sql = "select * From `user` where id = ?";
+
+console.log(sql.match(/(from|join)\s+([\w`\'\"]+)/i));
+
+const insert = "Insert into `user` (id, name) values (#{id}, #{name})";
+
+console.log(insert.match(/insert\sinto\s+([\w`\'\"]+)/i));
+
+const update = "Update `user` set name = #{name} where id = #{id}";
+
+console.log(update.match(/update\s+([\w`\'\"]+)/i));
+
+const delete1 = "Delete from `user` where id = #{id}";
+
+console.log(delete1.match(/delete\sfrom\s+([\w`\'\"]+)/i));
