a204b40ab08d7a78c191e9a6c3632dece61d0dee
speedphp <jake@speedphp.com>
2022-10-03 00:04:21 +0800

    整合前面研究的代码到model内，测试代码使用model之类成员方法

--- src/database/orm-decorator.ts
+++ src/database/orm-decorator.ts
@@ -41,168 +41,166 @@ export default class Model {
         //return await actionQuery('SELECT ' + fields + sql + sort + limit, params);
     }
 
-    async create(row) {
-        let sql = 'INSERT INTO ' + this.tableName
-            + ' (' + Object.keys(row).map((k) => '`' + k + '`').join(', ')
-            + ') VALUES (' + Object.keys(row).map((k) => ':' + k).join(', ')
-            + ')'
-        let res = await this.execute(sql, row)
-        return res
-    }
+    // async create(row) {
+    //     let sql = 'INSERT INTO ' + this.tableName
+    //         + ' (' + Object.keys(row).map((k) => '`' + k + '`').join(', ')
+    //         + ') VALUES (' + Object.keys(row).map((k) => ':' + k).join(', ')
+    //         + ')'
+    //     let res = await this.execute(sql, row)
+    //     return res
+    // }
 
-    async find(conditions, _sort, fields = '*') {
-        let res = await this.findAll(conditions, _sort, fields, 1)
-        return !lodash.isEmpty(res) ? res[0] : false
-    }
+    // async find(conditions, _sort, fields = '*') {
+    //     let res = await this.findAll(conditions, _sort, fields, 1)
+    //     return !lodash.isEmpty(res) ? res[0] : false
+    // }
 
-    async update(conditions, row) {
-        let [where, params] = this._where(conditions)
-        let values = {}
-        let sql = 'UPDATE ' + this.tableName
-            + ' SET ' + lodash.map(row, (v, k) => {
-                values[k] = v
-                return '`' + k + '` = :' + k
-            }).join(', ')
-            + where
-        let res = await this.execute(sql, lodash.merge(params, values))
-        return res
-    }
+    // async update(conditions, row) {
+    //     let [where, params] = this._where(conditions)
+    //     let values = {}
+    //     let sql = 'UPDATE ' + this.tableName
+    //         + ' SET ' + lodash.map(row, (v, k) => {
+    //             values[k] = v
+    //             return '`' + k + '` = :' + k
+    //         }).join(', ')
+    //         + where
+    //     let res = await this.execute(sql, lodash.merge(params, values))
+    //     return res
+    // }
 
-    async delete(conditions) {
-        let [where, params] = this._where(conditions)
-        let res = await this.execute('DELETE FROM ' + this.tableName + where, params)
-        return res
-    }
+    // async delete(conditions) {
+    //     let [where, params] = this._where(conditions)
+    //     let res = await this.execute('DELETE FROM ' + this.tableName + where, params)
+    //     return res
+    // }
 
-    async findCount(conditions) {
-        let [where, params] = this._where(conditions)
-        let res = await this.query('SELECT COUNT(*) AS M_COUNTER FROM ' + this.tableName + where, params)
-        return res[0]['M_COUNTER'] || 0
-    }
+    // async findCount(conditions) {
+    //     let [where, params] = this._where(conditions)
+    //     let res = await this.query('SELECT COUNT(*) AS M_COUNTER FROM ' + this.tableName + where, params)
+    //     return res[0]['M_COUNTER'] || 0
+    // }
 
-    async incr(conditions, field, optval = 1) {
-        let [where, params] = this._where(conditions)
-        let sql = 'UPDATE ' + this.tableName + ' SET `' + field + '` = `'
-            + field + '` + :M_INCR_VAL' + where
-        let res = await this.execute(sql, lodash.merge(params, { 'M_INCR_VAL': optval }))
-        return res
-    }
+    // async incr(conditions, field, optval = 1) {
+    //     let [where, params] = this._where(conditions)
+    //     let sql = 'UPDATE ' + this.tableName + ' SET `' + field + '` = `'
+    //         + field + '` + :M_INCR_VAL' + where
+    //     let res = await this.execute(sql, lodash.merge(params, { 'M_INCR_VAL': optval }))
+    //     return res
+    // }
 
-    decr(conditions, field, optval = 1) {
-        return this.incr(conditions, field, -optval)
-    }
+    // decr(conditions, field, optval = 1) {
+    //     return this.incr(conditions, field, -optval)
+    // }
 
-    pager(page, pageSize = 10, scope = 10, total) {
-        this._page = null
-        if (total === undefined) throw new Error('Pager total would not be undefined')
-        if (total > pageSize) {
-            let totalPage = Math.ceil(total / pageSize)
-            page = Math.min(Math.max(page, 1), total)
-            this._page = {
-                'total_count': total,
-                'pageSize': pageSize,
-                'totalPage': totalPage,
-                'firstPage': 1,
-                'prevPage': ((1 == page) ? 1 : (page - 1)),
-                'nextPage': ((page == totalPage) ? totalPage : (page + 1)),
-                'lastPage': totalPage,
-                'currentPage': page,
-                'allPages': [],
-                'offset': (page - 1) * pageSize,
-                'limit': pageSize
-            }
-            if (totalPage <= scope) {
-                this._page.allPages = lodash.range(1, totalPage)
-            } else if (page <= scope / 2) {
-                this._page.allPages = lodash.range(1, scope)
-            } else if (page <= totalPage - scope / 2) {
-                let right = page + (scope / 2)
-                this._page.allPages = lodash.range(right - scope + 1, right)
-            } else {
-                this._page.allPages = lodash.range(totalPage - scope + 1, totalPage)
-            }
-        }
-        return this._page
-    }
+    // pager(page, pageSize = 10, scope = 10, total) {
+    //     this._page = null
+    //     if (total === undefined) throw new Error('Pager total would not be undefined')
+    //     if (total > pageSize) {
+    //         let totalPage = Math.ceil(total / pageSize)
+    //         page = Math.min(Math.max(page, 1), total)
+    //         this._page = {
+    //             'total_count': total,
+    //             'pageSize': pageSize,
+    //             'totalPage': totalPage,
+    //             'firstPage': 1,
+    //             'prevPage': ((1 == page) ? 1 : (page - 1)),
+    //             'nextPage': ((page == totalPage) ? totalPage : (page + 1)),
+    //             'lastPage': totalPage,
+    //             'currentPage': page,
+    //             'allPages': [],
+    //             'offset': (page - 1) * pageSize,
+    //             'limit': pageSize
+    //         }
+    //         if (totalPage <= scope) {
+    //             this._page.allPages = lodash.range(1, totalPage)
+    //         } else if (page <= scope / 2) {
+    //             this._page.allPages = lodash.range(1, scope)
+    //         } else if (page <= totalPage - scope / 2) {
+    //             let right = page + (scope / 2)
+    //             this._page.allPages = lodash.range(right - scope + 1, right)
+    //         } else {
+    //             this._page.allPages = lodash.range(totalPage - scope + 1, totalPage)
+    //         }
+    //     }
+    //     return this._page
+    // }
 
-    _where(conditions) {
-        let result = [" ", {}]
-        if (typeof conditions === 'object' && !lodash.isEmpty(conditions)) {
-            let sql = null
-            if (conditions['where'] !== undefined) {
-                sql = conditions['where']
-                conditions['where'] = undefined
-            }
-            if (!sql) sql = Object.keys(conditions).map((k) => '`' + k + '` = :' + k).join(" AND ")
-            result[0] = " WHERE " + sql
-            result[1] = conditions
-        }
-        return result
-    }
+    // _where(conditions) {
+    //     let result = [" ", {}]
+    //     if (typeof conditions === 'object' && !lodash.isEmpty(conditions)) {
+    //         let sql = null
+    //         if (conditions['where'] !== undefined) {
+    //             sql = conditions['where']
+    //             conditions['where'] = undefined
+    //         }
+    //         if (!sql) sql = Object.keys(conditions).map((k) => '`' + k + '` = :' + k).join(" AND ")
+    //         result[0] = " WHERE " + sql
+    //         result[1] = conditions
+    //     }
+    //     return result
+    // }
 
-    query(_sql, _values) {
-        return this.execute(_sql, _values, true)
-    }
+    // query(_sql, _values) {
+    //     return this.execute(_sql, _values, true)
+    // }
 
-    async execute(_sql, _values, readonly = false) {
-        let connection: Connection;
-        const mysqlConfig = config("mysql");
-        if (readonly === true && mysqlConfig.slave !== undefined) {
-            let instanceKey = Math.floor(Math.random() * (mysqlConfig.slave.length))
-            connection = await this._db_instance(mysqlConfig.slave[instanceKey], 'slave_' + instanceKey)
-        } else {
-            connection = await this._db_instance(mysqlConfig, 'master')
-        }
-        let sql = connection.format(_sql, _values)
-        return await connection.query(sql);
-    }
+    // async execute(_sql, _values, readonly = false) {
+    //     let connection: Connection;
+    //     const mysqlConfig = config("mysql");
+    //     if (readonly === true && mysqlConfig.slave !== undefined) {
+    //         let instanceKey = Math.floor(Math.random() * (mysqlConfig.slave.length))
+    //         connection = await this._db_instance(mysqlConfig.slave[instanceKey], 'slave_' + instanceKey)
+    //     } else {
+    //         connection = await this._db_instance(mysqlConfig, 'master')
+    //     }
+    //     let sql = connection.format(_sql, _values)
+    //     return await connection.query(sql);
+    // }
 
-    async _db_instance(db_config, instance_key) {
-        if (db_instances[instance_key] === undefined) {
-            db_config.queryFormat = function (query, values) {
-                if (!values) return query
-                return query.replace(/\:(\w+)/g, function (txt, key) {
-                    if (values.hasOwnProperty(key)) {
-                        return this.escape(values[key])
-                    }
-                    return txt
-                }.bind(this))
-            }
-            db_instances[instance_key] = await createPool(db_config)
-        }
-        return db_instances[instance_key]
-    }
+    // async _db_instance(db_config, instance_key) {
+    //     if (db_instances[instance_key] === undefined) {
+    //         db_config.queryFormat = function (query, values) {
+    //             if (!values) return query
+    //             return query.replace(/\:(\w+)/g, function (txt, key) {
+    //                 if (values.hasOwnProperty(key)) {
+    //                     return this.escape(values[key])
+    //                 }
+    //                 return txt
+    //             }.bind(this))
+    //         }
+    //         db_instances[instance_key] = await createPool(db_config)
+    //     }
+    //     return db_instances[instance_key]
+    // }
 
     private where(conditions) {
-        let result = {sql: '1 ', values: {}};
+        const result = { sql: '1 ', values: {} };
         if (typeof conditions === 'object' && Object.keys(conditions).length > 0) {
             Object.keys(conditions).map((key) => {
-                if(typeof conditions[key] === 'object') {
-                    result = copyWhereStatsments(result, operatorFormat(key, conditions[key]));
-                }else{
-                    result["sql"] += ` AND ${key} = :${key}`
-                    result["values"][key] = conditions[key]
+                if (typeof conditions[key] === 'object') {
+                    result["sql"] += this.operatorFormat(key, conditions[key]);
+                    result["values"][key] = conditions[key]["$lt"];
+                } else {
+                    result["sql"] += ` AND ${key} = :${key}`;
+                    result["values"][key] = conditions[key];
                 }
             });
         }
         return result
     }
-}
 
-const copyWhereStatsments = (result, addiction) => {
-    return {
-        sql: result.sql + addiction.sql,
-        values: Object.assign(result.values, addiction.values)
+    private operatorFormat = (key, value) => {
+        let sql;
+        const operatorTemplate = { $lt: `< :{key}`, $lte: '<=', $gt: '>', $gte: '>=', $ne: '!=' };
+        Object.keys(value).map((operator) => {
+            if (operatorTemplate[operator]) {
+                sql += ` AND ${key} ${operatorTemplate[operator]} :${key}`;
+            }
+        });
+        return sql;
     }
 }
 
-const operatorFormat = (key, value) => {
-    const operator = {$lt: '<', $lte: '<=', $gt: '>', $gte: '>=', $ne: '!='};
-    return {
-        sql: ` AND ${key} ${operator[Object.keys(value)[0]]} :${key}`,
-        values: {[key]: value["$lt"]}
-    }
-}
 
 
 
