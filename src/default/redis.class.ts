d3a15d23ebb8cd1cfb87790de581d0fb47208a23
speedphp <jake@speedphp.com>
2022-10-03 23:29:24 +0800

    避免redis在启动的时候链接不上报错

--- src/default/redis.class.ts
+++ src/default/redis.class.ts
@@ -1,12 +1,18 @@
 import { bean, config } from "../speed";
 import IoRedis from "ioredis";
 
-export default class Redis extends IoRedis{
+export default class Redis extends IoRedis {
 
     @bean
-    public getRedis() : Redis {
+    public getRedis(): Redis {
+        if (config("redis") === undefined) {
+            return null;
+        }
         return new Redis(config("redis"));
     }
 
-    
+    constructor(config: any) {
+        super(config);
+    }
+
 }
