83ae990bc71d6644b2427ce90f0f2ce74a6f0d14
speedphp <jake@speedphp.com>
2023-06-21 11:18:42 +0800

    second page 测试完成，关于 header，文件上传和 jwt

--- test/second-page.test.ts
+++ test/second-page.test.ts
@@ -1,3 +1,5 @@
+import * as jwttoken from "jsonwebtoken";
+import * as fs from "fs";
 const chaiObj = require('chai');
 
 chaiObj.use(require("chai-http"));
@@ -15,9 +17,35 @@ describe("Sencond Page", () => {
         chaiObj.request("http://localhost:8081").get("/second/getCookie")
             .set("Cookie", "name=" + cookieSet).end((err, res) => {
                 chaiObj.assert.equal(res.text, "getCookie: " + cookieSet);
+                done();
+            });
+    });
+    it("/second/testSession", (done) => {
+        chaiObj.request("http://localhost:8081").get("/second/testSession").end((err, res) => {
+            chaiObj.assert.equal(res.text, "testForSession: 1");
             done();
         });
     });
+    it("/upload", (done) => {
+        const uploadfile = "./app/static/k.jpg";
+        chaiObj.request("http://localhost:8081").post("/upload")
+            .set('Content-Type', 'multipart/form-data')
+            .attach("file", uploadfile)
+            .end((err, res) => {
+                chaiObj.assert.equal(res.text, "upload success");
+                done();
+            });
+    });
+    it("/form", (done) => {
+        const token = jwttoken.sign({ foo: 'bar' }, 'shhhhhhared-secret');
+        const fileContents = fs.readFileSync("./app/src/views/upload.html", "utf8");
+        chaiObj.request("http://localhost:8081").post("/form")
+            .set({ "Authorization": `Bearer ${token}` })
+            .end((err, res) => {
+                chaiObj.assert.equal(res.text, fileContents);
+                done();
+            });
+    });
 });
 
-export {};
+export { };
