d0ea907b7b97e3c066ed0319627a465e80d0b3e3
speedphp <jake@speedphp.com>
2023-06-28 18:03:01 +0800

    改正 MQ 测试的方法

--- app/src/test-mq.class.ts
+++ app/src/test-mq.class.ts
@@ -16,51 +16,6 @@ export default class TestMq {
     @getMapping("/mq/sendByMQClass")
     async sendByMQClass() {
         this.rabbitMQ.send("myqueues", "hello world, by MQClass");
-        return "sent by MQClass";
-    }
-
-    @getMapping("/mq/sendByQueue")
-    async sendMq() {
-        const queue = 'myqueues';
-        const text = "hello world, by queue";
-        const connection = await connect({
-            protocol: 'amqp',
-            hostname: 'localhost',
-            port: 5672,
-            username: 'guest',
-            password: 'guest'
-        });
-        const channel = await connection.createChannel();
-        await channel.assertQueue(queue);
-        channel.sendToQueue(queue, Buffer.from(text));
-        await channel.close();
-        return "sent by queue";
-    }
-
-    @getMapping("/mq/sendByExchange")
-    async sendMq2() {
-        const exchange = 'myexchanges';
-        const text = "hello world, by exchange";
-        const connection = await connect('amqp://localhost');
-        const channel = await connection.createChannel();
-        await channel.checkExchange(exchange);
-        channel.publish(exchange, '', Buffer.from(text));
-        await channel.close();
-        return "sent by exchange";
-    }
-
-    @getMapping("/mq/listen")
-    async testMq() {
-        const connection = await connect('amqp://localhost');
-        const channel = await connection.createChannel();
-        const queue = 'myqueues';
-        const queue2 = 'myqueues2';
-        await channel.checkQueue(queue);
-        await channel.checkQueue(queue2);
-        await channel.consume(queue, (message) => {
-        }, { noAck: true });
-        await channel.consume(queue2, (message) => {
-        }, { noAck: true });
-        return "ok";
+        return "Sent by MQClass";
     }
 }
--- test/mq.test.ts
+++ test/mq.test.ts
@@ -6,27 +6,9 @@ const expect = chaiObj.expect;
 
 describe("Test RabbitMQ", () => {
     const testAddr = `http://${process.env.LOCAL_HOST || "localhost"}:8081`;
-    it("/mq/listen", (done) => {
-        chaiObj.request(testAddr).get("/mq/listen").end((err, res) => {
-            expect(res.text).to.be.equal("ok");
-            done();
-        });
-    });
     it("/mq/sendByMQClass", (done) => {
         chaiObj.request(testAddr).get("/mq/sendByMQClass").end((err, res) => {
-            expect(res.text).to.be.equal("sent by MQClass");
-            done();
-        });
-    });
-    it("/mq/sendByQueue", (done) => {
-        chaiObj.request(testAddr).get("/mq/sendByQueue").end((err, res) => {
-            expect(res.text).to.be.equal("sent by queue");
-            done();
-        });
-    });
-    it("/mq/sendByExchange", (done) => {
-        chaiObj.request(testAddr).get("/mq/sendByExchange").end((err, res) => {
-            expect(res.text).to.be.equal("sent by exchange");
+            expect(res.text).to.be.equal("Sent by MQClass");
             done();
         });
     });
