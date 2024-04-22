4c8b28709cd7e40eface2b346a3f635b2fa3e540
speedphp <jake@speedphp.com>
2024-04-22 21:31:32 +0800

    修复socket.io未从bean工厂获取装饰类的问题

--- src/default/socket-io.class.ts
+++ src/default/socket-io.class.ts
@@ -1,5 +1,6 @@
 import { Server as IoServer } from "socket.io";
 import { createServer } from "http";
+import { getComponent } from "../core.decorator";
 
 let io: IoServer = null;
 const listeners = { "event": [], "disconnect": null, "error": null, "connected": null };
@@ -9,27 +10,27 @@ class SocketIo {
     public static setIoServer(app, ioSocketConfig) {
         const httpServer = createServer(app);
         io = new IoServer(httpServer, ioSocketConfig);
-        io.use((socket, next) => {
+        io.use(async (socket, next) => {
             if (listeners["connected"] !== null) {
-                listeners["connected"](socket, async (err) => {
+                await getComponent(listeners["connected"].target.constructor)[listeners["connected"].propertyKey](socket, async (err) => {
                     if (listeners["error"] !== null && err) {
-                        await listeners["error"](socket, err);
+                        await getComponent(listeners["error"].target.constructor)[listeners["error"].propertyKey](socket, err);
                     }
                 });
             }
             next();
         });
-        io.on("connection", (socket) => {
+        io.on("connection", async (socket) => {
             if (listeners["disconnect"] !== null) {
                 socket.on("disconnect", async (reason) => {
-                    await listeners["disconnect"](socket, reason);
+                    await getComponent(listeners["disconnect"].target.constructor)[listeners["disconnect"].propertyKey](socket, reason);
                 });
             }
             socket.use(async ([event, ...args], next) => {
                 try {
                     for (let listener of listeners["event"]) {
                         if (listener[1] === event) {
-                            await listener[0](socket, ...args);
+                            await getComponent(listener[0].target.constructor)[listener[0].propertyKey](socket, ...args);
                         }
                     }
                 } catch (err) {
@@ -39,7 +40,7 @@ class SocketIo {
 
             if (listeners["error"] !== null) {
                 socket.on("error", async (err) => {
-                    await listeners["error"](socket, err);
+                    await getComponent(listeners["error"].target.constructor)[listeners["error"].propertyKey](socket, err);
                 });
             }
         });
@@ -48,20 +49,32 @@ class SocketIo {
 
     public static onEvent(event: string) {
         return (target: any, propertyKey: string) => {
-            listeners["event"].push([target[propertyKey], event]);
+            listeners["event"].push([{
+                target: target,
+                propertyKey: propertyKey
+            }, event]);
         }
     }
 
     public static onError(target: any, propertyKey: string) {
-        listeners["error"] = target[propertyKey];
+        listeners["error"] = {
+            target: target,
+            propertyKey: propertyKey
+        };
     }
 
     public static onDisconnect(target: any, propertyKey: string) {
-        listeners["disconnect"] = target[propertyKey];
+        listeners["disconnect"] = {
+            target: target,
+            propertyKey: propertyKey
+        };
     }
 
     public static onConnected(target: any, propertyKey: string) {
-        listeners["connected"] = target[propertyKey];
+        listeners["connected"] = {
+            target: target,
+            propertyKey: propertyKey
+        };
     }
 }
 
